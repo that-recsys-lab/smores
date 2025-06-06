4161c9db22e22c51a2dc29a42b25b39707706850
Robin Burke <rburke2233@users.noreply.github.com>
2025-06-06 10:06:45 -0600

    Deleting unused files

--- smores/stakeholders/item_choice_model.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ItemChoiceModel(ABC):
-    @abstractmethod
-    def select_item(self, items, threshold, category_preferences, prohibited_genres):
-        pass
