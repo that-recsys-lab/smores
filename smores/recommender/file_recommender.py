4fb7c708bb53a4da06e525a40dc33375f72feeb0
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-08-12 17:46:29 -0600

    Update file_recommender.py
    Change list of items to dictionary mapping item to probability, use probability in choice

--- smores/recommender/file_recommender.py
+++ smores/recommender/file_recommender.py
@@ -1,14 +1,17 @@
-from lenskit.data.items import ItemList
 import csv
+from collections import defaultdict
+from lenskit.data.items import ItemList
+
 import smores
 from smores.recommender import Recommender, RecommenderFactory
+
 
 class FileBasedRecommender(Recommender):
     """Recommender that load items from a file"""
     
     def __init__(self):
         super().__init__()
-        self.items: List[int] = []
+        self.items = defaultdict(float)
         self.file_path = None
         # Needs no training
         self.trained = True
@@ -30,7 +33,7 @@ class FileBasedRecommender(Recommender):
             reader = csv.DictReader(f)
             for row in reader:
                 if 'item_id' in row:
-                    self.items.append(int(row['item_id']))
+                    self.items[int(row['item_id'])] = float(row['probability'])
                         
             smores.Smores.state.logger.info(f"Loaded {len(self.items)} items")
 # This error should be fatal
@@ -38,7 +41,7 @@ class FileBasedRecommender(Recommender):
  #           smores.Smores.state.logger.error(f"Error loading file {self.file_path}")
 
     def check_items(self):
-        filtered_items = [item_id for item_id in self.items \
+        filtered_items = [item_id for item_id in self.items.keys() \
                       if smores.Smores.state.items.exists_item(item_id)]
         return filtered_items
 
@@ -54,7 +57,7 @@ class FileBasedRecommender(Recommender):
     def get_recommendations(self, user_id) -> ItemList:
         prior_interactions = self.get_user(user_id)
         if prior_interactions is not None and len(prior_interactions) > 0:
-            usable_items = [item for item in self.items if item not in prior_interactions]
+            usable_items = {item: value for item,value in self.items() if item not in prior_interactions}
         else:
             usable_items = self.items
 
@@ -62,7 +65,7 @@ class FileBasedRecommender(Recommender):
         if len(usable_items) < slate_size:
             slate_size = len(usable_items)
         
-        items = smores.Smores.state.rand.choice(usable_items, slate_size)
+        items = smores.Smores.state.rand.choice(usable_items.keys(), slate_size, p=usable_items.values())
         
         scores = [1.0] * slate_size
         ranks = list(range(1, slate_size + 1))
