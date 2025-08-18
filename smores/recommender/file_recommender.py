4b41a4ac506c1c95dccddd589d30bafada1ac0d5
e-mckinnie <elizabeth.mckinnie@outlook.com>
2025-08-18 16:12:02 -0600

    Update file_recommender.py
    Fix iteration through dictionary

--- smores/recommender/file_recommender.py
+++ smores/recommender/file_recommender.py
@@ -41,7 +41,7 @@ class FileBasedRecommender(Recommender):
  #           smores.Smores.state.logger.error(f"Error loading file {self.file_path}")
 
     def check_items(self):
-        filtered_items = {item: value for item,value in self.items \
+        filtered_items = {item: value for item,value in self.items.items() \
                       if smores.Smores.state.items.exists_item(item)}
         return filtered_items
 
@@ -57,7 +57,7 @@ class FileBasedRecommender(Recommender):
     def get_recommendations(self, user_id) -> ItemList:
         prior_interactions = self.get_user(user_id)
         if prior_interactions is not None and len(prior_interactions) > 0:
-            usable_items = {item: value for item,value in self.items if item not in prior_interactions}
+            usable_items = {item: value for item,value in self.items.items() if item not in prior_interactions}
         else:
             usable_items = self.items
 
